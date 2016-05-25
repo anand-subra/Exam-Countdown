a38f38c9580ae3b3f25b96e40ff4582d565dd206
Anand Subramaniam <anand.subra@hotmail.com>
2016-05-25 21:58:58 +0100

    Add existing files

---
+++ js/process.js
@@ -0,0 +1,54 @@
+// $('#test-count').countdown('2016/04/15 22:32:50+01:00', function(event) {
+//   $(this).html(event.strftime('%D <span class="time">days</span> %H <span class="time">hours</span> %M <span class="time">mins</span> %S <span class="time">secs</span>'));
+//
+//   if ($('#test-count:contains("00 days 00 hours 00 mins 00 secs")').length > 0) {
+//     $(this).html(event.strftime("<span class='complete'>EXAM COMPLETE!</span>"));
+//     $('.test').css("opacity", "0.8");
+//     $('.test').css("color", "grey");
+//   }
+//
+// });
+
+$('#ID-count').countdown('2016/05/25 14:30:00+01:00', function(event) {
+  $(this).html(event.strftime('%D <span class="time">days</span> %H <span class="time">hours</span> %M <span class="time">mins</span> %S <span class="time">secs</span>'));
+
+  if ($('#ID-count:contains("00 days 00 hours 00 mins 00 secs")').length > 0) {
+    $(this).html(event.strftime("<span class='complete'>EXAM COMPLETE!</span>"));
+    $('.ID').css("opacity", "0.8");
+    $('.ID').css("color", "grey");
+  }
+
+});
+
+$('#DMSN-count').countdown('2016/05/31 10:00:00+01:00', function(event) {
+  $(this).html(event.strftime('%D <span class="time">days</span> %H <span class="time">hours</span> %M <span class="time">mins</span> %S <span class="time">secs</span>'));
+
+  if ($('#ID-count:contains("00 days 00 hours 00 mins 00 secs")').length > 0) {
+    $(this).html(event.strftime("<span class='complete'>EXAM COMPLETE!</span>"));
+    $('.DMSN').css("opacity", "0.8");
+    $('.DMSN').css("color", "grey");
+  }
+
+});
+
+$('#WP-count').countdown('2016/06/02 10:00:00+01:00', function(event) {
+  $(this).html(event.strftime('%D <span class="time">days</span> %H <span class="time">hours</span> %M <span class="time">mins</span> %S <span class="time">secs</span>'));
+
+  if ($('#ID-count:contains("00 days 00 hours 00 mins 00 secs")').length > 0) {
+    $(this).html(event.strftime("<span class='complete'>EXAM COMPLETE!</span>"));
+    $('.WP').css("opacity", "0.8");
+    $('.WP').css("color", "grey");
+  }
+
+});
+
+$('#PD-count').countdown('2016/06/03 10:00:00+01:00', function(event) {
+  $(this).html(event.strftime('%D <span class="time">days</span> %H <span class="time">hours</span> %M <span class="time">mins</span> %S <span class="time">secs</span>'));
+
+  if ($('#ID-count:contains("00 days 00 hours 00 mins 00 secs")').length > 0) {
+    $(this).html(event.strftime("<span class='complete'>EXAM COMPLETE!</span>"));
+    $('.PD').css("opacity", "0.8");
+    $('.PD').css("color", "grey");
+  }
+
+});
